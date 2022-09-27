cf309ee1c1668c513be67de7e49d823ffa8242b4
Макс Добжинецький Добжинецький <maks.dobzhinetsky14@gmail.com>
2022-09-27 15:50:26 +0300

    foo

--- articles/tests.py
+++ articles/tests.py
@@ -1,9 +1,6 @@
 import pytest
 
-from .models import Article
-
 
 @pytest.mark.django_db
 def test_capital_case():
-    qs = Article.objects.filter(id=1)
-    assert qs.title == 'qq'
+    assert 'qq' == 'qq'
